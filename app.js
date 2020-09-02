50bd19fdbc4b24d42f51d1c475d05d3f6373ae3a
Hakkani <refathasan5124@gmail.com>
2020-09-02 23:13:42 +0900

    30. Sending Responses

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+const { write } = require("fs");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  // process.exit()
   console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<header><title>My First Page</title></header>");
+  res.write("<body>");
+  res.write("<h1>Hello form node.js </h1>");
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
